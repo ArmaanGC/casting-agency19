d523453d5ea51ba46fdb92f602c026153b0d63ac
Armaan <armaankaurgill@gmail.com>
2022-01-03 20:13:34 +0530

    Updating databse name

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export DATABASE_URL="postgres://mdrkdxvvuyyjco:50c06bf15c1356e00e096e59fce26acddd55fea1f9a0f61e959a691016e1289b@ec2-3-227-154-49.compute-1.amazonaws.com:5432/dc64nhadl24odh"
+export DATABASE_URL="postgres://pdwptvgutkzryv:9412ade8f0b38973cf7d7d1219e9673d588c89e0f92eac3a5a1ae81eaefa2a9f@ec2-54-236-156-167.compute-1.amazonaws.com:5432/d6hmdtl72hg1j"
 export EXCITED="true"
 echo "setup.sh script executed successfully!"
